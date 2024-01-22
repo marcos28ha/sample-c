ad16e0faaa4707fe4d1ad8c1c20c34487d46e1b3
marcos28ha <marcos.h.a8@gmail.com>
2024-01-22 16:21:45 +0100

    sample

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 4.0;
+    double resultado = sqrt(x);
+
+    printf("La raíz cuadrada de %f es %f\n", x, resultado);
+
+    return 0;
+}
